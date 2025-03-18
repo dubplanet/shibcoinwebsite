b1a0bfd8b3a13af9efc6f9af83d6e9f0132950c8
dubplanet <hparsonsmoney@gmail.com>
2025-03-18 18:11:40 -0400

    image

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const CACHE_URLS = [
     '/index.html',
     '/styles.css',
     '/script.js',
-    '/images/shiba-inu-shib-logo.png',
+    'shiba-inu-shib-logo.png',
     'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
